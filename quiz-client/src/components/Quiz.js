59290387a89d7af1282b9bc608978cef26578f61
Kagenuge <kaarlen@gmail.com>
2019-11-19 16:43:35 +0200

    get request working

--- quiz-client/src/components/Quiz.js
+++ quiz-client/src/components/Quiz.js
@@ -1,20 +1,32 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import TriviaBox from './TriviaBox';
 import NextButton from './NextButton';
 import '../style/Quiz.css';
 import buttonSound from '../sounds/hastalavista.mp3';
+import { fetchData } from '../serviceClient';
 
 const Quiz = () => {
   const [hastalavista] = useState(new Audio(buttonSound));
+  const [questions, setQuestions] = useState([{
+    question: "noo?",
+    choices: ["o","a","a","r"]
+  }]);
+
+  const getData = () => {
+    fetchData().then(res => setQuestions(res));
+  };
 
   useEffect(() => {
     hastalavista.play();
-}, [])
+    getData();
+  }, [])
+  console.log(questions)
 
-  return(
+  return (
     <div className="quizPage">
-    <h1 className="quizTitle">Schwarzenegger Quiz</h1>
-      <TriviaBox />
+      <h1 className="quizTitle"> Arnold Schwarzenegger </h1>
+      <h1>Quiz</h1>
+      <TriviaBox data={questions} />
       <NextButton />
     </div>
   )
