62fd02ab5f6550f33ba9b68a63f2c609629a2c70
Kagenuge <kaarlen@gmail.com>
2019-11-19 13:55:23 +0200

    sounds work

--- quiz-client/src/components/Intro.js
+++ quiz-client/src/components/Intro.js
@@ -1,37 +1,44 @@
-import React, { Component } from 'react'
+import React, {useState, useEffect} from 'react'
 import '../style/Intro.css'
 import Arnold from '../pics/arnold.jpg'
-import IntroMusic from '../sounds/introVer1.mp3'
+import introMusic from '../sounds/introVer1.mp3';
 import { Link } from 'react-router-dom';
 
-export default class Intro extends Component {
-    render() {
-        return (
-            <div className="introDiv">
+const Intro = () => {
+    const [introAudio] = useState(new Audio(introMusic));
 
+    useEffect(() => {
+        introAudio.play();
+    }, [])
+
+    return (
+        <div className="introDiv">
+            <div></div>
             <section className="introSection">
-        <img src={Arnold} className="introImg" />
-        <h1 className="introH1">
-            <span className="introSpan">S</span>
-            <span className="introSpan">C</span>
-            <span className="introSpan">H</span>
-            <span className="introSpan">W</span>
-            <span className="introSpan">A</span>
-            <span className="introSpan">R</span>
-            <span className="introSpan">Z</span>
-            <span className="introSpan">E</span>
-            <span className="introSpan">N</span>
-            <span className="introSpan">E</span>
-            <span className="introSpan">G</span>
-            <span className="introSpan">G</span>
-            <span className="introSpan">E</span>
-            <span className="introSpan">R</span>
-            <br />
-            <span className="introSpan">QUIZ</span>
-        </h1>
-        <Link to="/quiz"><button className="introButton"><span className="introSpan">PLAY</span></button></Link>
-    </section>
-            </div>
-        )
-    }
+                <img src={Arnold} className="introImg" />
+                <h1 className="introH1">
+                    <span className="introSpan">S</span>
+                    <span className="introSpan">C</span>
+                    <span className="introSpan">H</span>
+                    <span className="introSpan">W</span>
+                    <span className="introSpan">A</span>
+                    <span className="introSpan">R</span>
+                    <span className="introSpan">Z</span>
+                    <span className="introSpan">E</span>
+                    <span className="introSpan">N</span>
+                    <span className="introSpan">E</span>
+                    <span className="introSpan">G</span>
+                    <span className="introSpan">G</span>
+                    <span className="introSpan">E</span>
+                    <span className="introSpan">R</span>
+                    <br />
+                    <span className="introSpan">QUIZ</span>
+                </h1>
+                <Link to="/quiz"><button className="introButton"><span className="introSpan">PLAY</span></button></Link>
+            </section>
+        </div>
+    )
 }
+
+
+export default Intro;
--- quiz-client/src/components/Quiz.js
+++ quiz-client/src/components/Quiz.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import TriviaBox from './TriviaBox';
 import NextButton from './NextButton';
 import '../style/Quiz.css';
+import buttonSound from '../sounds/hastalavista.mp3';
 
 const Quiz = () => {
+  const [hastalavista] = useState(new Audio(buttonSound));
+
+  useEffect(() => {
+    hastalavista.play();
+}, [])
+
   return(
     <div className="quizPage">
     <h1 className="quizTitle">Schwarzenegger Quiz</h1>
