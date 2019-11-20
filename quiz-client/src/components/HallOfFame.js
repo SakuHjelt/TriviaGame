63463fe86ebda4032a441168e6685770cc1fdc18
Saku Hjelt <sakuhjelt@gmail.com>
2019-11-20 16:43:43 +0200

    last push for wed for sync

--- quiz-client/src/components/HallOfFame.js
+++ quiz-client/src/components/HallOfFame.js
@@ -1,40 +1,42 @@
-import React, { Component } from 'react'
-import '..style/HallOfFame.css'
-import { rightAnswer } from './TriviaQuestion'
-export default class HallOfFame extends Component {
-    state = { name: '', score: { rightAnswer } }
-    handleNameChange = (e) => {
-        this.setState({ name: e.targetvalue })
-    }
+// import React, { Component } from 'react'
+// import '..style/HallOfFame.css'
+// import {  } from './TriviaQuestion'
 
-    handleCreateClick = e => {
-        e.preventDefault();
-        PostScore(this.state).then(response => {
-            this.setState({ name: 'name', score: '' });
-            this.props.history.push('/GameOver');
-        })
-    }
 
-    render() {
-        return (
-            <div className="addScoreDiv">
-                <form>
-                    <label className="fameLabel">Name:</label>
-                    <br />
-                    <input className="fameInput" type='text'
-                        placeholder='_ _ _ _ _ _ _ _'
-                        id='fameId'
-                        value={this.state.name}
-                        onChange={this.handleNameChange}
-                        required='required' />
-                    <label className="yourScore">Your Score:</label>
-                    <button className="fameButton"
-                        type='input'
-                        onClick={this.handleCreateClick}
-                    >| To The Hall Of Fame |</button>
-                </form>
+// export default class HallOfFame extends Component {
+//     state = { name: '', score: { count } }
+//     handleNameChange = (e) => {
+//         this.setState({ name: e.targetvalue })
+//     }
 
-            </div>
-        )
-    }
-}
+//     handleCreateClick = e => {
+//         e.preventDefault();
+//         PostScore(this.state).then(response => {
+//             this.setState({ name: 'name', score: '' });
+//             this.props.history.push('/GameOver');
+//         })
+//     }
+
+//     render() {
+//         return (
+//             <div className="addScoreDiv">
+//                 <form>
+//                     <label className="fameLabel">Name:</label>
+//                     <br />
+//                     <input className="fameInput" type='text'
+//                         placeholder='_ _ _ _ _ _ _ _'
+//                         id='fameId'
+//                         value={this.state.name}
+//                         onChange={this.handleNameChange}
+//                         required='required' />
+//                     <label className="yourScore">Your Score:</label>
+//                     <button className="fameButton"
+//                         type='input'
+//                         onClick={this.handleCreateClick}
+//                     >| To The Hall Of Fame |</button>
+//                 </form>
+
+//             </div>
+//         )
+//     }
+// }
