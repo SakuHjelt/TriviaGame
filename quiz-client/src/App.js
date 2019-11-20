d614a00b040884ea134008b6d047976546c6c1ef
Saku Hjelt <sakuhjelt@gmail.com>
2019-11-20 13:19:47 +0200

    pushed for sync

--- quiz-client/src/App.js
+++ quiz-client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Quiz from './components/Quiz';
 import Intro from './components/Intro';
 import './style/App.css';
+import GameOver from './components/GameOver'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
@@ -11,6 +12,8 @@ function App() {
         <Switch>
           <Route exact path="/" component={Intro} />
           <Route exact path="/quiz" component={Quiz} />
+          <Route exact path="/gameover" component={GameOver} />
+
         </Switch>
       </Router>
     </div>
